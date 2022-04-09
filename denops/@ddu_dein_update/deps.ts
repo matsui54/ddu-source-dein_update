70bd4818465c9fb6c0ebb5efa79f99967b7a7f15
matsui54 <haru.matu9168@gmail.com>
2022-04-09 21:37:04 +0900

    Update deps

--- denops/@ddu_dein_update/deps.ts
+++ denops/@ddu_dein_update/deps.ts
@@ -4,16 +4,16 @@ export {
   gather,
 } from "https://deno.land/x/denops_std@v3.3.0/batch/mod.ts";
 export * as op from "https://deno.land/x/denops_std@v3.3.0/option/mod.ts";
-export * as path from "https://deno.land/std@0.133.0/path/mod.ts";
+export * as path from "https://deno.land/std@0.134.0/path/mod.ts";
 export * as fn from "https://deno.land/x/denops_std@v3.3.0/function/mod.ts";
 export * as nvimFn from "https://deno.land/x/denops_std@v3.3.0/function/nvim/mod.ts";
 export * as helper from "https://deno.land/x/denops_std@v3.3.0/helper/mod.ts";
 export * as vars from "https://deno.land/x/denops_std@v3.3.0/variable/mod.ts";
 export * as autocmd from "https://deno.land/x/denops_std@v3.3.0/autocmd/mod.ts";
 export { isLike } from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
-export * from "https://deno.land/x/ddu_vim@v1.4.0/types.ts";
+export * from "https://deno.land/x/ddu_vim@v1.5.0/types.ts";
 export * as buffer from "https://deno.land/x/denops_std@v3.3.0/buffer/mod.ts";
 export {
   abortable,
   pooledMap,
-} from "https://deno.land/std@0.133.0/async/mod.ts";
+} from "https://deno.land/std@0.134.0/async/mod.ts";
