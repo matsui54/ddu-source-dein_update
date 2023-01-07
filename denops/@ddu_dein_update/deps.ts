f3817aadbb3f00759e5c32c37f1e390c607d1a31
matsui54 <haru.matu9168@gmail.com>
2023-01-07 16:53:13 +0900

    Remove github workflow

--- denops/@ddu_dein_update/deps.ts
+++ denops/@ddu_dein_update/deps.ts
@@ -4,16 +4,16 @@ export {
   gather,
 } from "https://deno.land/x/denops_std@v3.12.1/batch/mod.ts";
 export * as op from "https://deno.land/x/denops_std@v3.12.1/option/mod.ts";
-export * as path from "https://deno.land/std@0.170.0/path/mod.ts";
+export * as path from "https://deno.land/std@0.171.0/path/mod.ts";
 export * as fn from "https://deno.land/x/denops_std@v3.12.1/function/mod.ts";
 export * as nvimFn from "https://deno.land/x/denops_std@v3.12.1/function/nvim/mod.ts";
 export * as helper from "https://deno.land/x/denops_std@v3.12.1/helper/mod.ts";
 export * as vars from "https://deno.land/x/denops_std@v3.12.1/variable/mod.ts";
 export * as autocmd from "https://deno.land/x/denops_std@v3.12.1/autocmd/mod.ts";
 export { isLike } from "https://deno.land/x/unknownutil@v2.1.0/mod.ts";
-export * from "https://deno.land/x/ddu_vim@v2.0.0/types.ts";
+export * from "https://deno.land/x/ddu_vim@v2.1.0/types.ts";
 export * as buffer from "https://deno.land/x/denops_std@v3.12.1/buffer/mod.ts";
 export {
   abortable,
   pooledMap,
-} from "https://deno.land/std@0.170.0/async/mod.ts";
+} from "https://deno.land/std@0.171.0/async/mod.ts";
