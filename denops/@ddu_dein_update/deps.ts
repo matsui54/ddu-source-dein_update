2cd26b27793f835c0a3bd80c7212432259cf4a22
matsui54 <haru.matu9168@gmail.com>
2023-03-27 17:12:38 +0900

    📦 Update Deno dependencies

--- denops/@ddu_dein_update/deps.ts
+++ denops/@ddu_dein_update/deps.ts
@@ -1,19 +1,19 @@
-export type { Denops } from "https://deno.land/x/denops_std@v4.0.0/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v4.1.0/mod.ts";
 export {
   batch,
   gather,
-} from "https://deno.land/x/denops_std@v4.0.0/batch/mod.ts";
-export * as op from "https://deno.land/x/denops_std@v4.0.0/option/mod.ts";
-export * as path from "https://deno.land/std@0.173.0/path/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v4.0.0/function/mod.ts";
-export * as nvimFn from "https://deno.land/x/denops_std@v4.0.0/function/nvim/mod.ts";
-export * as helper from "https://deno.land/x/denops_std@v4.0.0/helper/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v4.0.0/variable/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v4.0.0/autocmd/mod.ts";
+} from "https://deno.land/x/denops_std@v4.1.0/batch/mod.ts";
+export * as op from "https://deno.land/x/denops_std@v4.1.0/option/mod.ts";
+export * as path from "https://deno.land/std@0.181.0/path/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v4.1.0/function/mod.ts";
+export * as nvimFn from "https://deno.land/x/denops_std@v4.1.0/function/nvim/mod.ts";
+export * as helper from "https://deno.land/x/denops_std@v4.1.0/helper/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v4.1.0/variable/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v4.1.0/autocmd/mod.ts";
 export { isLike } from "https://deno.land/x/unknownutil@v2.1.0/mod.ts";
-export * from "https://deno.land/x/ddu_vim@v2.2.0/types.ts";
-export * as buffer from "https://deno.land/x/denops_std@v4.0.0/buffer/mod.ts";
+export * from "https://deno.land/x/ddu_vim@v2.7.0/types.ts";
+export * as buffer from "https://deno.land/x/denops_std@v4.1.0/buffer/mod.ts";
 export {
   abortable,
   pooledMap,
-} from "https://deno.land/std@0.173.0/async/mod.ts";
+} from "https://deno.land/std@0.181.0/async/mod.ts";
